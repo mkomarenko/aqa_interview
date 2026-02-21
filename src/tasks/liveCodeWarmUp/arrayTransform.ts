c6955b87dbb9b2a340f4a1bee077ff152a240f82
Maksym Komarenko <max.komarenko@gmail.com>
2026-02-21 23:39:12 +0200

    Add array transform + filter (live coding warm up) task

---
+++ src/tasks/liveCodeWarmUp/arrayTransform.ts
@@ -0,0 +1,8 @@
+// Given an array of users, return emails of active users in lowercase. Complete it in two lines of code
+
+const users = [
+  { email: "A@Mail.com", active: true },
+  { email: "B@Mail.com", active: false },
+  { email: "C@Mail.com", active: true }
+];
+
